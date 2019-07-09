d27da19f8ce7ba617591e59433752ef812747062
andrefangeloni <andrefangeloni@gmail.com>
2019-07-08 22:37:10 -0300

    Implementado agenda e início das notificações

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,12 +1,35 @@
 import * as Yup from "yup";
-import { startOfHour, parseISO, isBefore } from "date-fns";
-import Appointment from "../models/Appointment";
+import { startOfHour, parseISO, isBefore, format } from "date-fns";
+import pt from "date-fns/locale/pt";
 import User from "../models/User";
+import File from "../models/File";
+import Appointment from "../models/Appointment";
+import Notification from "../schemas/Notification";
 
 class AppointmentController {
   async index(req, res) {
+    const { page = 1 } = req.query;
+
     const appointments = await Appointment.findAll({
-      where: { user_id: req.userId, canceled_at: null }
+      where: { user_id: req.userId, canceled_at: null },
+      order: ["date"],
+      attributes: ["id", "date"],
+      limit: 20,
+      offset: (page - 1) * 20,
+      include: [
+        {
+          model: User,
+          as: "provider",
+          attributes: ["id", "name"],
+          include: [
+            {
+              model: File,
+              as: "avatar",
+              attributes: ["id", "path", "url"]
+            }
+          ]
+        }
+      ]
     });
 
     return res.json(appointments);
@@ -67,6 +90,21 @@ class AppointmentController {
       user_id: req.userId,
       provider_id,
       date
+    });
+
+    /**
+     * Notify appointment provider
+     */
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' H:mm'h'",
+      { locale: pt }
+    );
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para ${formattedDate}`,
+      user: provider_id
     });
 
     return res.json(appointment);
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import SessionController from "./app/controllers/SessionController";
 import FileController from "./app/controllers/FileController";
 import ProviderController from "./app/controllers/ProviderController";
 import AppointmentController from "./app/controllers/AppointmentController";
+import ScheduleController from "./app/controllers/ScheduleController";
 import authMiddleware from "./app/middlewares/auth";
 
 const routes = new Router();
@@ -13,8 +14,6 @@ const upload = multer(multerConfig);
 
 routes.post("/users", UserController.store);
 routes.post("/sessions", SessionController.store);
-routes.post("/files", upload.single("file"), FileController.store);
-routes.post("/appointments", AppointmentController.store);
 
 routes.use(authMiddleware);
 
@@ -22,5 +21,11 @@ routes.put("/users", UserController.update);
 
 routes.get("/providers", ProviderController.index);
 routes.get("/appointments", AppointmentController.index);
+
+routes.post("/appointments", AppointmentController.store);
+
+routes.get("/schedule", ScheduleController.index);
+
+routes.post("/files", upload.single("file"), FileController.store);
 
 export default routes;
